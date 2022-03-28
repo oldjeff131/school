7bc7339f5beea2242d7e551c3564ebfcc6dd80a2
oldjeff131 <oldjeff131@gmail.com>
2022-03-28 22:33:01 +0800

    updata coda

--- opencv/homework/homeework.py
+++ opencv/homework/homeework.py
@@ -2,7 +2,7 @@ import sys
 import cv2 as cv
 import numpy as np
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow,QMenu,QAction
+from PyQt5.QtWidgets import QApplication, QMainWindow,QAction,QMenu
 
 class Window(QMainWindow):
     def __init__(self,parent=None): #視窗建立
@@ -10,30 +10,31 @@ class Window(QMainWindow):
         self.setWindowTitle("test")
         self.resize(1000,800)
         self._createMenuBar()
-        self._createToolBar()
 
     def _createMenuBar(self): #選單
         menuBar=self.menuBar()
-        fileMenu=QMenu("&File",self)
-        menuBar.addMenu(fileMenu)
-        SettingMenu=menuBar.addMenu("&Setting")
-        ImageMenu=menuBar.addMenu("&Image Processing")
-    
-    def _createToolBar(self):
-        fileToolbar=self.addToolBar("File")
-        editToolbar=QToolBar("Setting",self)
-        self.addToolBar(editToolBar)
-        helpToolBar = QToolBar("Image Processing", self)
-        self.addToolBar(Qt.LeftToolBarArea, helpToolBar)
+        fileMenu=menuBar.addMenu('File')
+        OpenImageMenu=QMenu("&OpenImage",self)
+        fileMenu.addMenu(OpenImageMenu)
 
-    def _createActions(self):
-        self.newAction=QAction(self)
-        self.newAction.setText("&OpenImage")
-        self.ROIAction=QAction("&ROI",self)
-        self.IHAction=QAction("&Image histogram",self)
-        self.CSAction=QAction("&Color space",self)
-        self.THAction = QAction("&Thresholding", self)
-        self.HEAction = QAction("&Histogram Equalization", self)
+        SettingMenu=menuBar.addMenu("&Setting")
+        ROIMenu=QMenu("&ROI",self)
+        SettingMenu.addMenu(ROIMenu)
+        IHAMenu=QMenu("&Image histogram",self)
+        SettingMenu.addMenu(IHAMenu)
+        Image_histogramMenu=QMenu("&Color space",self)
+        SettingMenu.addMenu(Image_histogramMenu)
+
+        ImageMenu=menuBar.addMenu("&Image Processing")
+        Color_spaceMenu=QMenu("&Color spaceMenu",self)
+        ImageMenu.addMenu(Color_spaceMenu)
+        ThresholdingMenu=QMenu("&Thresholding",self)
+        ImageMenu.addMenu(ThresholdingMenu)
+        Histogram_EqualizationMenu=QMenu("&Histogram Equalization",self)
+        ImageMenu.addMenu(Histogram_EqualizationMenu)
+
+    def open_image(self):
+        
 
 if __name__=="__main__":
     app=QApplication(sys.argv)

67c165946ae923719f664e408f2a1daf66e717a6
oldjeff131 <oldjeff131@gmail.com>
2022-04-28 17:02:11 +0800

    add some coda

--- sort/Merge_Sort.py
+++ sort/Merge_Sort.py
@@ -2,21 +2,18 @@ import random
 import time
 def merge(left, right):
     result = []
-
     while len(left) and len(right):
         if (left[0] < right[0]):
             result.append(left.pop(0))
         else:
             result.append(right.pop(0))
-
     result = result+left if len(left) else result+right
     return result
 
 def mergeSort(array):
-    if len(array) < 2:
+    if len(array) < 2:#當array小於2時直接跳出
         return array
-
-    mid = len(array)//2
+    mid = len(array)//2 #取得list長度中間值
     leftArray = array[:mid]
     rightArray = array[mid:]
 
--- sort/Quick_Sort.py
+++ sort/Quick_Sort.py
@@ -2,25 +2,24 @@ import random
 import time
 def partition(arr, low, high):
     i = (low-1)        
-    pivot = arr[high] 
+    pivot = arr[high] #設定基準點的值
     for j in range(low, high):
-        if arr[j] <= pivot:
-            i = i+1
+        if arr[j] <= pivot:#找尋比基準點小的或相同的值
+            i = i+1 
             arr[i], arr[j] = arr[j], arr[i]
- 
-    arr[i+1], arr[high] = arr[high], arr[i+1]
+    arr[i+1], arr[high] = arr[high], arr[i+1]#兩數值互換
     return (i+1)
  
-def quickSort(arr, low, high):
+def quickSort(arr, low, high):#arr陣列,low開始位子,high結束位子
     if len(arr) == 1: #如果arr式的長度為1，直接回傳
         return arr
     if low < high: 
         pi = partition(arr, low, high)
-        quickSort(arr, low, pi-1)
-        quickSort(arr, pi+1, high)
+        quickSort(arr, low, pi-1) #處理較小部分的子循環
+        quickSort(arr, pi+1, high) #處理較大部分的子循環
 
 start = time.perf_counter()#開始時間
-arr = [random.randrange(1,1000) for i in range(1, 1001)] #產生一千筆變數
+arr = [random.randrange(1,5) for i in range(1, 6)] #產生一千筆變數
 n = len(arr) #計算arr長度
 print("排序前:")
 for i in range(n): #排序前的顯示
@@ -36,6 +35,6 @@ for i in range(n): #排序後的顯示
     else:
         print("%s\n" % arr[i])
 
-print("This time is being calculated")
+print("總使用時間:")
 end = time.perf_counter()#結束時間
 print(end - start)

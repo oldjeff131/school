87c7abed78671e852573b4d3bab2a7ff334b7ae1
oldjeff131 <oldjeff131@gmail.com>
2022-04-29 12:52:58 +0800

    finish merge &quick

--- sort/Merge_Sort.py
+++ sort/Merge_Sort.py
@@ -3,21 +3,20 @@ import time
 def merge(left, right):
     result = []
     while len(left) and len(right):
-        if (left[0] < right[0]):
+        if (left[0] < right[0]):#判斷哪個比較小，小的放入result的list的第一位
             result.append(left.pop(0))
         else:
             result.append(right.pop(0))
-    result = result+left if len(left) else result+right
+    result = result+left if len(left) else result+right #排入result的list 如果已經放入左方數值 則放入右方數字
     return result
 
 def mergeSort(array):
     if len(array) < 2:#當array小於2時直接跳出
         return array
     mid = len(array)//2 #取得list長度中間值
-    leftArray = array[:mid]
-    rightArray = array[mid:]
-
-    return merge(mergeSort(leftArray),mergeSort(rightArray))
+    leftArray = array[:mid]#取得左邊arr的所有數值
+    rightArray = array[mid:]#取得右邊arr的所有數值
+    return merge(mergeSort(leftArray),mergeSort(rightArray))#切到左右邊個只剩下一個數值
 
 start = time.perf_counter()#開始時間
 arr=[random.randrange(1,1000) for i in range(1, 1001)] #產生一千筆變數
@@ -29,13 +28,13 @@ for i in range(n): #排序前的顯示
     else:
         print("%s\n" % arr[i])
 mergeSort(arr)
-print("合併排列後:")
+print("合併排序後:")
 for i in range(n): #排序後的顯示
     if i<n-1:
         print("%s" % arr[i],end=' ')
     else:
         print("%s\n" % arr[i])
 
-print("總使用時間:")
+print("合併排序總使用時間:")
 end = time.perf_counter()#結束時間
 print(end - start)
--- sort/Quick_Sort.py
+++ sort/Quick_Sort.py
@@ -6,9 +6,9 @@ def partition(arr, low, high):
     for j in range(low, high):
         if arr[j] <= pivot:#找尋比基準點小的或相同的值
             i = i+1 
-            arr[i], arr[j] = arr[j], arr[i]
+            arr[i], arr[j] = arr[j], arr[i]#兩數值互換
     arr[i+1], arr[high] = arr[high], arr[i+1]#兩數值互換
-    return (i+1)
+    return (i+1) #回傳基準點最後交換的位子
  
 def quickSort(arr, low, high):#arr陣列,low開始位子,high結束位子
     if len(arr) == 1: #如果arr式的長度為1，直接回傳
@@ -19,7 +19,7 @@ def quickSort(arr, low, high):#arr陣列,low開始位子,high結束位子
         quickSort(arr, pi+1, high) #處理較大部分的子循環
 
 start = time.perf_counter()#開始時間
-arr = [random.randrange(1,5) for i in range(1, 6)] #產生一千筆變數
+arr = [random.randrange(1,1000) for i in range(1, 1001)] #產生一千筆變數
 n = len(arr) #計算arr長度
 print("排序前:")
 for i in range(n): #排序前的顯示
@@ -28,13 +28,13 @@ for i in range(n): #排序前的顯示
     else:
         print("%s\n" % arr[i])
 quickSort(arr, 0, n-1) #呼叫快速排序
-print("快速排列後:")
+print("快速排序後:")
 for i in range(n): #排序後的顯示
     if i<n-1:
         print("%s" % arr[i],end=' ')
     else:
         print("%s\n" % arr[i])
 
-print("總使用時間:")
+print("快速排序總使用時間:")
 end = time.perf_counter()#結束時間
 print(end - start)
